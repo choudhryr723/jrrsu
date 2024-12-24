c77c71cbb8f0b8f45b4e2c5dc5113bf032a3705b
RaKeSH KuMaR ChOuDhArY <45654946+choudhryr723@users.noreply.github.com>
2024-12-24 18:10:03 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,20 @@
-const path = require('path');
-
 module.exports = {
-  entry: './src/index.js',  // Your entry point JavaScript file
-  output: {
-    filename: 'bundle.js',  // Output bundled file
-    path: path.resolve(__dirname, 'dist'),  // Output directory
-  },
+  // Your existing Webpack configuration...
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'], // For importing CSS
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader', // To handle JS/ES6 features
-        },
+        test: require.resolve('jquery'),
+        use: [
+          {
+            loader: 'expose-loader',
+            options: 'jQuery',
+          },
+          {
+            loader: 'expose-loader',
+            options: '$',
+          },
+        ],
       },
     ],
-  },
-  resolve: {
-    alias: {
-      jquery: 'jquery/src/jquery',  // Ensures jQuery is correctly loaded
-    },
   },
 };
